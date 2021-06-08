852ca8678d1dc821a710ba2848dbe8daaeb3b344
User Friendly <pivanov@foobar.ninja>
2021-06-08 19:40:04 -0400

    LeetCode: easy, array, rotate, part 2
    Still trying to do that O(1) memory and O(n) time

--- leetcode/easy/array/rotate.js
+++ leetcode/easy/array/rotate.js
@@ -10,6 +10,62 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
+	if (k > nums.length) {
+		k = k % nums.length
+	}
+	
+	if (nums.length == 1 || nums.length == k || k == 0)
+		return
+	
+	// Buffer
+	let b = []
+	// Current position
+	let c = 0
+	
+	let divisible = false
+	
+	/*if (nums.length % k == 0) {
+		divisible = true
+	}
+	else */if (nums.length % 2 == 0 && k % 2 == 0) {
+		divisible = true
+	}
+	else if (nums.length % 3 == 0 && k % 3 == 0) {
+		divisible = true
+	}
+	
+	//console.log(`divisible: ${divisible}`)
+	
+	let looped = false
+	
+	b.push(nums[0])
+	for (let i = 0; i < nums.length; i++) {
+		// Offset current position by rotation
+		c += k
+		
+		// Overflow check
+		if (c >= nums.length) {
+			c -= nums.length
+			if (divisible && i > 0) {
+				looped = true
+			}
+		}
+		
+		// Swap
+		b.push(nums[c])
+		nums[c] = b.shift()
+		
+		if (divisible && looped && i > 0) {
+			c++
+			b = [nums[c]]
+			looped = false
+		}
+		
+		//console.log(`step (${i}): c: ${c}, b: ${b}, nums: ${nums}`)
+	}
+};
+
+function slowStableRotate(nums, k) {
 	if (nums.length == k || k == 0)
 		return
 	
@@ -24,6 +80,55 @@ var rotate = function(nums, k) {
 		nums[0] = prev
 	}
 };
+
+function runTestSuite() {
+	let n_max = 128
+	let k = 0
+	
+	let generate_data = function(n) {
+		let data = []
+		for (let i = 0; i < n; i++)
+				data[i] = i + 1
+		return data
+	}
+	
+	main_loop:
+	for (let n = 0; n <= n_max; n++) {
+		for (let k = 0; k < n_max; k++) {
+			
+			let data = generate_data(n)
+			let expected = data.slice()
+			
+			slowStableRotate(expected, k)
+			
+			rotate(data, k)
+			
+			if (false === data.every((v, i) => v == expected[i])) {
+				console.log(`fail: rotate: array(${n}, ${k})`)
+				console.log(`expected: ${expected}, ret: ${data}`)
+				break main_loop
+			}
+		}
+	}
+}
+
+runTestSuite()
+
+function runTests2() {
+	let k = 4
+	let n = 6
+	let data = []
+	for (let i = 0; i < n; i++)
+		data[i] = i + 1
+	
+	console.log(data)
+	
+	rotate(data, k)
+	
+	console.log(data)
+}
+
+// runTests2()
 
 function runTests() {
 	let data = [1, 2, 3, 4, 5, 6, 7]
@@ -40,7 +145,7 @@ function runTests() {
 	}
 }
 
-runTests()
+// runTests()
 
 /*import { rotateTestData as bigTestData } from './rotateTestData.js'
 
